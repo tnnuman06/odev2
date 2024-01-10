1d27facafab68ea3cf24d3d6ee8f110a568140e9
Numan Tan <146818406+tnnuman06@users.noreply.github.com>
2024-01-10 23:46:20 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -4,104 +4,45 @@ namespace ödev_oyun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Şehir tahmin oyunumuza hoşgeldiniz.\n Hazırsanız başlayalım.");
-            Console.ReadKey();
-            int hak = 0, sehir, sayac = 5;
-            Random rasgele = new Random();
-            sehir = rasgele.Next(1, 5);
-            do
+            Console.WriteLine("How many numbers do you want to sort?");
+            int count = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter starting number:");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter end number:");
+            int end = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+            int[] numbers = new int[count];
+
+            for (int i = 0; i < numbers.Length; i++)
             {
-                if (sehir == 1)
-                {
-                    Console.WriteLine("Çiğ köftesiyle meşhur şehir");
-                    string cevap1 = Console.ReadLine();
-                    if (cevap1 == "Şanlıurfa")
-                    {
-                        hak += 10;
-                        Console.WriteLine("Tebrikler");
-                        Console.WriteLine($"Skorunuz: {hak}");
-                        Random rasgele2 = new Random();
-                        sehir = rasgele2.Next(2, 5);
-                    }
-                    else
-                    {
-                        sayac--;
-                        Console.WriteLine("Yanlış cevap :( Biraz daha düşün al sana bir ipucu\n ...lıyım ezelden");
-                    }
-                }
-                if (sehir == 2)
-                {
-                    Console.WriteLine("Bu şehir hamsiyi çok sever");
-                    string cevap2 = Console.ReadLine();
-                    if (cevap2 == "Trabzon")
-                    {
-                        hak += 10;
-                        Console.WriteLine("Tebrikler");
-                        Console.WriteLine($"Skorunuz: {hak}");
-                        Random rasgele3 = new Random();
-                        sehir = rasgele3.Next(3, 5);
-                    }
-                    else
-                    {
-                        sayac--;
-                        Console.WriteLine("Yanlış cevap :( Biraz daha düşün al sana bir ipucu\n 4 büyüklerde takımı var");
-                    }
-                }
-                if (sehir == 3)
-                {
-                    Console.WriteLine("Bu şehir çok kalabalık?");
-                    string cevap3 = Console.ReadLine();
-                    if (cevap3 == "İstanbul")
-                    {
-                        hak += 10;
-                        Console.WriteLine("Tebrikler");
-                        Console.WriteLine($"Skorunuz: {hak}");
-                        Random rasgele4 = new Random();
-                        sehir = rasgele4.Next(4, 5);
-                    }
-                    else
-                    {
-                        sayac--;
-                        Console.WriteLine("Yanlış cevap :( Biraz daha düşün al sana bir ipucu\n Trafik çok yoğun");
-                    }
-                }
-                if (sehir == 4)
-                {
-                    Console.WriteLine("Bu şehir kayısısı ile meşhur");
-                    string cevap4 = Console.ReadLine();
-                    if (cevap4 == "Malatya")
-                    {
-                        hak += 10;
-                        Console.WriteLine("Tebrikler");
-                        Console.WriteLine($"Skorunuz: {hak}");
-                        Random rasgele5 = new Random();
-                        sehir = rasgele5.Next(5, 5);
-                    }
-                    else
-                    {
-                        sayac--;
-                        Console.WriteLine("Yanlış cevap :( Biraz daha düşün al sana bir ipucu\n ...ya ...ya eşin");
-                    }
-                }
-                if (sehir == 5)
-                {
-                    Console.WriteLine("Bu şehir Türkiye'nin merkezinde?");
-                    string cevap5 = Console.ReadLine();
-                    if (cevap5 == "Ankara")
-                    {
-                        hak += 10;
-                        Console.WriteLine("Tebrikler");
-                        Console.WriteLine($"Skorunuz: {hak}");
-                    }
-                    else
-                    {
-                        sayac--;
-                        Console.WriteLine("Yanlış cevap :( Biraz daha düşün al sana bir ipucu\n Bu şehir bi başka Başkent");
-                    }
-                }
+                numbers[i] = 0;
             }
-            while (sayac > 0);
-            Console.WriteLine($"{hak}");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int number;
+                bool unique;
+                do
+                {
+                    number = rnd.Next(start, end);
+                    unique = true;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (numbers[j] == number)
+                        {
+                            unique = false;
+                            break;
+                        }
+                    }
+                } while (!unique);
+                numbers[i] = number;
+            }
+            Array.Sort(numbers);
+            Console.WriteLine("Print sequential numbers:");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
         }
     }
 }
